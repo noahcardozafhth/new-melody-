8d914182583870d851a3cbd5e77781cae218b2c5
noahcardozafhth <112573403+noahcardozafhth@users.noreply.github.com>
2022-09-23 12:21:59 -0400

    Add files via upload

---
+++ Arduino-Melody/Arduino-Melody.ino
@@ -0,0 +1,107 @@
+/*
+  Based on Tom Igoe's code
+  http://www.arduino.cc/en/Tutorial/Tone
+*/
+
+#include "pitches.h"
+
+const int buzzerPin = 2;
+const int buttonPin = 5;     // the number of the pushbutton pin
+const int led = 3;
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+int melody[] = {
+  
+  NOTE_AS4,8, NOTE_AS4,8, NOTE_AS4,8,//1
+  NOTE_F5,2, NOTE_C6,2,
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
+  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,8, NOTE_C5,8, NOTE_C5,8,
+  NOTE_F5,2, NOTE_C6,2,
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
+  
+  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4, //8  
+  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,-8, NOTE_C5,16, 
+  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C5,-8, NOTE_C5,16,
+  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+  
+  
+  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C6,-8, NOTE_C6,16,
+  NOTE_F6,4, NOTE_DS6,8, NOTE_CS6,4, NOTE_C6,8, NOTE_AS5,4, NOTE_GS5,8, NOTE_G5,4, NOTE_F5,8,
+  NOTE_C6,1
+  
+};
+
+int noteDurations[] = {
+  8, 8, 8, 8, 8, 8, 4, 
+  4, 4, 4, 4,
+  6, 8, 4, 4,
+  4, 4, 8, 6,
+};
+
+int beatsPerMinute = 10;
+
+void setup() {
+  // initialize the LED pin as an output:
+ 
+  pinMode(buttonPin, INPUT);
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+ if (buttonState == HIGH) {
+ digitalWrite(led, HIGH);
+ delay(1000);
+playMelody();
+  } else {
+ digitalWrite(led, LOW);
+ delay(1000);
+  }
+}
+    
+ int tempo = 108;
+
+// change this to whichever pin you want to use
+int buzzer = 11;
+
+
+
+
+
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+
+void playMelody() {
+  // iterate over the notes of the melody: 
+int len = sizeof(melody)/sizeof(melody[0]);
+for (int thisNote = 0; thisNote < len; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = static_cast<int>(1000 / noteDurations[thisNote] * 60 / beatsPerMinute);
+    tone(buzzerPin, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(buzzerPin);
+  }
+  // Pause before playing again
+  delay(1000);
+}
